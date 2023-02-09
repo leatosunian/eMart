0a1bbac3ffd26dc5dc0b46cd4fe61cef4214d8e5
Lea <leandrotosunian@hotmail.com>
2023-02-09 16:01:16 -0300

    s

--- controllers/productController.js
+++ controllers/productController.js
@@ -193,9 +193,10 @@ const deleteVariantByStock = async (req, res) => {
 }
 
 const addImageInGallery = async (req, res) => {
+    console.log(req.files.image.path);
     const imgPath = req.files.image.path.split('\\')
     const imgString = imgPath[2]
-    console.log(req.files.image.path);
+    
     const image = new ProductGallery(req.body)
 
     image.image = imgString
